8a07a38dcd7ae4cb6e5abad1f865f9e97250c224
Andy <andy.greenhaw@gmail.com>
2021-01-22 16:07:37 -0600

    Trying a new routing technique.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import { BrowserRouter, Route, Link } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import MyPortfolio from "./pages/Portfolio";
-import Navbar from "./components/Navbar/Navbar.js"
+import Navbar from "./components/Navbar/Navbar.js";
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 
 import "./App.css"
 
@@ -14,11 +14,14 @@ function App() {
           <Navbar />
         </div>
         <div className="mainSection">
+          <Switch>
             <Route exact path="/" component={Homepage} />
+            
             {/* <Route exact path="/andy-greenhaw-development-portfolio" component={Homepage} /> */}
 
             {/* NON NAV PAGES */}
             <Route exact path="/portfolio" component={MyPortfolio} />
+          </Switch>
         </div>
       </div>
     </BrowserRouter>
