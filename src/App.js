771b59a37744d5bee2a6774812c99f7909e9d9db
Andy <andy.greenhaw@gmail.com>
2021-01-22 15:42:11 -0600

    Added base url

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import "./App.css"
 function App() {
 
   return(
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <div className="container-fluid">
       <div className="navbarSection">
         <Navbar />
