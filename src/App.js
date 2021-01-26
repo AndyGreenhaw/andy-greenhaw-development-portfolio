23bbdf31587e9f76d39740ffd9729d9508a79865
Andy <andy.greenhaw@gmail.com>
2021-01-26 15:06:08 -0600

    Working on routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import Homepage from "./pages/Homepage";
 import MyPortfolio from "./pages/Portfolio";
 import Navbar from "./components/Navbar/Navbar.js";
-import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 import "./App.css"
 
 function App() {
 
   return(
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <Router basename={process.env.PUBLIC_URL}>
       <div className="container-fluid">
         <div className="navbarSection">
           <Navbar />
@@ -20,11 +20,11 @@ function App() {
             {/* <Route exact path="/andy-greenhaw-development-portfolio" component={Homepage} /> */}
 
             {/* NON NAV PAGES */}
-            <Route exact path="/portfolio" component={MyPortfolio} />
+            <Route path="/portfolio" component={MyPortfolio} />
           </Switch>
         </div>
       </div>
-    </BrowserRouter>
+    </Router>
   )
 }
 
